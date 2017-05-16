0ec3c34d4f7226340242fc04d96bd98aacb293c7
Nicholas Major <nick@nmajor.com>
2017-05-16 11:39:31 +0100

    Added CRUD for restaurants controllers and views

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  def rating_good?
+    if (rating)
+      rating > 4
+    else  
+      false
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,42 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :restaurants, except: [:delete]
+
+  # # READ
+  # # GET a list of restaurants
+  # get "restaurants", to: "restaurants#index"
+
+  # # GET a single restaurant
+  # get "restaurants/:id", to: "restaurants#show"
+
+
+  # # CREATE
+  # # Display the restaurant FORM 
+  # get "restaurants/new", to: "restaurants#new"
+  
+  # # Process the new form
+  # post "restaurants", to: "restaurants#create"
+
+  # #UPDATE
+  # # Display the update form
+  # get "restaurants/:id/edit", to: "restaurants#edit"
+
+  # # Process the update form
+  # put "restaurants/:id", to: "restaurants#update"
+  # patch "restaurants/:id", to: "restaurants#update"
+
+
+  # # DELETE
+  # delete "restaurants/:id", to: "restaurants#destroy"
 end
+
+
+
+
+
+
+
+
+
+
+
